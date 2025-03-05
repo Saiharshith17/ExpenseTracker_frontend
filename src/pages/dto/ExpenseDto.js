0f478014fc9e9f6a148838aac89e69578138cf26
Saiharshith17 <eedarasaiharshith6699@gmail.com>
2025-03-06 00:43:07 +0530

    added dto

---
+++ src/pages/dto/ExpenseDto.js
@@ -0,0 +1,8 @@
+// ExpenseDto.js (used to define the structure of your expense data)
+export const ExpenseDto = {
+  key: 0,
+  amount: 0,
+  merchant: '',
+  currency: '',
+  createdAt: ''
+};
---
+++ src/pages/dto/UserDto.js
@@ -0,0 +1,9 @@
+// UserDto.js
+export const UserDto = {
+    userId: '',
+    firstName: '',
+    lastName: '',
+    phoneNumber: 0,
+    email: '',
+    profilePic: '' // optional
+};
